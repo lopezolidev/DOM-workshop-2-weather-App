905728952c15979e9c0e225cd3a2a1b1a091aa99
rockwatch <rocksistor@gmail.com>
2022-11-24 15:03:04 -0400

    basic styling of cards for climate app

---
+++ src/assets/logic.js
@@ -0,0 +1,7 @@
+//Tasks:
+//1 - initialize variables for API and DOM elements
+//2 - set-up basic events that the DOM will trigger
+//3 - write  async functions
+//3.1 - create DOM nodes with classes
+//3.2 - build logic to render card with user input (city)
+
