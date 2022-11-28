764685264af9c26ad9f21990525093f8047afd1b
rockwatch <rocksistor@gmail.com>
2022-11-27 20:15:52 -0400

    finished logic for adding and deleting cards. Following up with styling of each card and testing .composedPath() feature for .path substitution

--- src/assets/logic.js
+++ src/assets/logic.js
@@ -1,10 +1,14 @@
 //Tasks:
-//1 - initialize variables for API and DOM elements
-//2 - set-up basic events that the DOM will trigger
-//2.1 - install fetch for async calls with fetch API
-//3 - write  async functions
-//3.1 - create DOM nodes with classes
-//3.2 - build logic to render card with user input (city)
+//1 - initialize variables for API and DOM elements ← DONE
+//2 - set-up basic events that the DOM will trigger ← DONE
+//2.1 - install fetch for async calls with fetch API ← DONE
+//3 - write  async functions ← DONE
+//3.1 - create DOM nodes with classes ← DONE
+//3.2 - build logic to render card with user input (city) ← DONE
+//4 - build logic check for repeated cards ← DONE
+//5 - delete card logic ← DONE
+//6 - add styles to app design ← TO-DO
+
 
 //input event:
 const input = document.querySelector('#input');
@@ -16,7 +20,7 @@ const page = document.querySelector('.cards-section')
 
 const cardsArray = [];
 
-const iconArray = [];
+const iconArray = []; //TO-DO
 
 async function fetchData(e){
    try{
@@ -26,14 +30,13 @@ async function fetchData(e){
          const location = message.toUpperCase();
          //transforming to uppercase value from input for async call
 
-
          const raw = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=46326a6829dc87953c417b28d2a4194a&units=metric`);
          //fetching raw data from weather app
 
          const parsed = await raw.json();
          //parsing in json() raw weater data
 
-         console.log(parsed)
+         // console.log(parsed)
 
          //=====================Let's create HTML nodes
 
@@ -93,6 +96,7 @@ async function fetchData(e){
          //city name
 
          //ICON → Later in CSS insert ICON
+         deleteIcon.addEventListener('click', deleteCard)
 
          degrees.innerText = parsed.main.temp
          //city degrees
@@ -103,11 +107,12 @@ async function fetchData(e){
          climateDescription.innerText = parsed.weather[0].description;
          //climate description
 
-         isAppended(wrapper);
-         //call isAppended to check if its repeated
+         const result = isAppended(wrapper);
+         //storing result from isAppended function using wrapper as our argument
 
-         console.log(wrapper, cardsArray);
-         return wrapper;
+         appendCard(result, wrapper);
+         //appendCard will use the result and the wrapper element for validation and insert the element in the DOM
+         
 
       } catch(e) {
     throw new Error(e);
@@ -115,17 +120,44 @@ async function fetchData(e){
 }
 
 function isAppended(arg){
-   if(cardsArray.some(item => item.cardTitle.innerText == arg.cardTitle.innerText)){
-      console.log('arg:', arg)
+   return cardsArray.some(i => i.children[0].innerText == arg.children[0].innerText);
+
+   //arg.then(e => e.children[0].innerText)
+
+   //as arg is wrapper, children stands for the HTML elements array and [0] is the cardTitle element. innerText is the object of comparison with any element that already exists inside the cards array
+}
+//isAppended function will return true if there's at least one element equal to those in the array
+
+function appendCard(proof, element){
+   if(proof){
       return;
    } else {
-      page.append(arg);
+      cardsArray.push(element);
+      //pushing elements inside the cardsArray
+
+      page.append(element);
+      //appending such element in the page
+
+      console.log(cardsArray)
    }
+}
+//appendCard function will take as argument isAppended function to validate the presence of a card already pushed to the array of cards to decide if its appended to the container or not
+
+function deleteCard(e){
+   const elementName = e.target.parentElement.parentElement.children[0].children[0].innerText 
+   //name of city accessed through parent element
+
+   const indexOfElement = cardsArray.findIndex(i => i.children[0].children[0].innerText == elementName)
+   //knowing which is the index of the card that triggered the event
+
+   cardsArray.splice(indexOfElement, 1)
+   //eliminating card of the array, to erase the reference
+
+   e.path[2].remove();
+   //removing card using path[2], referencing the whole card
 }
 
 submitButton.addEventListener('click', fetchData)
 submitButton.addEventListener('click', (e) => e.preventDefault()) //to not reload the page
 
 
-
-// fetchData('caracas');
